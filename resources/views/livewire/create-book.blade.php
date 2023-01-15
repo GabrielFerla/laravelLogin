4d2d252ecbc52ee54c4a1d7fb7ae9935c12d4517
Gabriel Ferla <gabriel.ferla@devsquad.com>
2023-01-15 10:31:27 -0300

    change return

--- resources/views/livewire/create-book.blade.php
+++ resources/views/livewire/create-book.blade.php
@@ -31,7 +31,7 @@
                 </div>
 
                 <div class="flex items-center justify-start space-x-4">
-                    <a href="/user" class="text-gray-900  font-medium  text-sm ">Back</a>
+                    <a href="/book" class="text-gray-900  font-medium  text-sm ">Back</a>
                     <button type="submit"
                     class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 ml-3 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">
                         Save
